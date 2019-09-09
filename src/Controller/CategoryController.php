3eb10ed536b0ad6631341e8df3caa2c782ae2efd
skillup <myEmail@example.com>
2019-09-09 21:13:52 +0300

    sort by atttributes

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use Sonata\AdminBundle\Form\Type\Filter\ChoiceType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
@@ -22,10 +25,13 @@ class CategoryController extends AbstractController
     /**
      * @Route("/category/{id}", name="category_show")
      */
-    public function show(Category $category)
-    {
+    public function show(Category $category, Request $request, ProductRepository $productRepository)
+    {  $form = $this->getSearchForm($category);
+        $form->handleRequest($request);
+        $products = $productRepository->findByFilter($category, $form->getData());
         return $this->render('category/show.html.twig', [
-            'category' => $category,
+            'category' => $products,
+            'form' => $form->createView(),
         ]);
     }
 
@@ -36,4 +42,22 @@ class CategoryController extends AbstractController
         ]);
     }
 
+    private function getSearchForm(Category $category)
+    {
+        $formBuilder = $this->createFormBuilder();
+        $formBuilder->setMethod('GET');
+
+        foreach ($category->getAttributes() as $attribute) {
+            $values = $attribute->getValuesList();
+            $choices = array_combine($values, $values);
+
+            $formBuilder->add('attr'.$attribute->getId(), ChoiceType::class, [
+                'multiple' => true,
+                'expanded' =>true,
+                'choices' => $choices,
+                'label' => $attribute->getName(),
+            ]);
+        }
+        return $formBuilder->getForm();
+    }
 }
