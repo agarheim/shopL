a1069a824d82759f9099033c5cb1e2fbc3777fc8
romanr <agarheim@gmail.com>
2019-09-18 17:10:24 +0300

    WFP1

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Controller;
-
 use App\Entity\Order;
 use App\Entity\OrderItem;
 use App\Entity\Product;
@@ -19,107 +17,81 @@ use WayForPay\SDK\Exception\ApiException;
 use WayForPay\SDK\Wizard\CheckWizard;
 use WayForPay\SDK\Wizard\PurchaseWizard;
 use WayForPay\SDK\Domain\Product as WayForPayProduct;
-
 class OrderController extends AbstractController
 {
-
     /**
      * @Route("/add-to-cart/{id}", name="order_add_to_cart")
      */
     public function addToCart(Product $product, OrderService $orderService, Request $request)
     {
         $orderService->add($product, 1, $this->getUser());
-
         if ($request->isXmlHttpRequest()) {
             return $this->headerCart($orderService);
         }
-
         return $this->redirectToRoute('default');
     }
-
     public function headerCart(OrderService $orderService)
     {
         $order = $orderService->getOrder();
-
         return $this->render('order/header_cart.html.twig', [
             'order' => $order,
         ]);
     }
-
     /**
      * @Route("/cart", name="order_cart")
      */
     public function cart(OrderService $orderService)
     {
         $order = $orderService->getOrder();
-
         return $this->render('order/cart.html.twig', [
             'order' => $order,
         ]);
     }
-
     /**
      * @Route("/cart/{id}/count", name="order_set_count")
      */
-    public function showCount(OrderItem $item, OrderService $orderService, Request $request)
-    {
-        $cartTable=  $this->setCount($item, $orderService, $request);
-        $h_c=$this->headerCart($orderService);
-        return $this->json(['cartTable' => $cartTable, 'headerCart' => $h_c]);
-
-    }
     public function setCount(OrderItem $item, OrderService $orderService, Request $request)
-    { $count = $request->request->getInt('count');
+    {
+        $count = $request->request->getInt('count');
         $order = $orderService->getOrder();
-
         if ($count > 0 && $item->getCart() === $order) {
             $item->setCount($count);
-            $orderService->save($item->getCart(),$this->getUser());
+            $orderService->save($item->getCart(), $this->getUser());
         }
         return $this->render('order/_cart_table.html.twig', [
             'order' => $order,
         ]);
-
     }
-
     /**
      * @Route("/cart/{id}/delete", name="order_delete_item")
      */
     public function deleteItem(OrderItem $item, OrderService $orderService)
     {
         $order = $orderService->getOrder();
-
         if ($item->getCart() === $order) {
             $orderService->deleteItem($item);
         }
-
         return $this->render('order/_cart_table.html.twig', [
             'order' => $order,
-
         ]);
     }
-
     /**
-
      * @Route("/cart/order", name="order_make_order")
      */
-    public  function makeOrder(OrderService $orderService, Request $request)
+    public function makeOrder(OrderService $orderService, Request $request)
     {
-        $order= $orderService->getOrder();
+        $order = $orderService->getOrder();
         $form = $this->createForm(OrderType::class, $order);
         $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $orderService->makeOrder($order);
-            return $this->redirectToRoute('order_success');
+            return $this->redirectToRoute('order_success', ['id' => $order->getId()]);
         }
-        return $this->render('order/make_order.html.twig',[
-            'order'=> $order,
-            'form'=>$form->createView(),
+        return $this->render('order/make_order.html.twig', [
+            'order' => $order,
+            'form' => $form->createView(),
         ]);
-
     }
-
     /**
      * @Route("/order/success/{id}", name="order_success")
      */
